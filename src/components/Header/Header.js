ba8871947754216446f43dc1eded7f15d1be8566
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-12 10:57:16 +0300

    Minor changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router';
 import styles from './Header.less';
 import withStyles from '../../decorators/withStyles';
 import { Nav, Navbar, CollapsibleNav } from 'react-bootstrap';
-// import { NavItemLink } from 'react-router-bootstrap';
 
 @withStyles(styles)
 export default class Header {
@@ -14,7 +13,7 @@ export default class Header {
     return (
       <header>
         <Navbar fixedTop brand={<Link to="/">Toni Karttunen</Link>} toggleNavKey={0}>
-          <CollapsibleNav eventKey={0}> {/* Reference for toggling the nav */}
+          <CollapsibleNav eventKey={0}> {/* A reference for toggling the nav */}
             <Nav navbar right>
               <li><Link to="/">Home</Link></li>
               <li><Link to="projects">Projects</Link></li>
