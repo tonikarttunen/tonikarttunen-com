e6956747cf9da755c999cf8ca77d521644351231
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-18 01:39:27 +0300

    Add content to home page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,7 +10,11 @@ export default class Contact {
     return (
       <section className='Contact container'>
         <h1>Contact</h1>
-        <span className='icon-500px'></span> 500px
+        <span className='ion-android-mail'/> Mail
+        <span className='ion-social-github'/> GitHub
+        <span className='icon-500px'/> 500px
+        <span className='ion-social-instagram'/> Instagram
+        <span className='ion-social-twitter'/> Twitter
       </section>
     );
   }
