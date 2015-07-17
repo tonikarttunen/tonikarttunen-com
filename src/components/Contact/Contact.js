ed0358ab53700156f4ed6edcd360ee06d767885b
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-17 22:29:54 +0300

    Remove DocumentTitle from Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,6 @@
 // Contact
 
 import React from 'react/addons';
-import DocumentTitle from 'react-document-title';
 import styles from './Contact.less';
 import withStyles from '../../decorators/withStyles';
 
@@ -9,12 +8,10 @@ import withStyles from '../../decorators/withStyles';
 export default class Contact {
   render() {
     return (
-      <DocumentTitle title='Contact - Toni Karttunen'>
-        <section className='Contact container'>
-          <h1>Contact</h1>
-          <span className='icon-500px'></span> 500px
-        </section>
-      </DocumentTitle>
+      <section className='Contact container'>
+        <h1>Contact</h1>
+        <span className='icon-500px'></span> 500px
+      </section>
     );
   }
 }
