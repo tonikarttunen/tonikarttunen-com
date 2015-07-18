526c8b4f170d0b8608222eca74e851e28ac4e6e6
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-18 05:19:04 +0300

    Configure router and header for 1-page content structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import React from 'react/addons';
 import { Link } from 'react-router';
 import styles from './Header.less';
 import withStyles from '../../decorators/withStyles';
-import { Nav, Navbar, CollapsibleNav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 
 @withStyles(styles)
 export default class Header {
@@ -12,14 +12,7 @@ export default class Header {
   render() {
     return (
       <header className='Header'>
-        <Navbar fixedTop brand={<Link to='/'>Toni Karttunen</Link>} toggleNavKey={0}>
-          <CollapsibleNav eventKey={0}> {/* A reference for toggling the nav */}
-            <Nav navbar right>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/projects'>Projects</Link></li>
-            </Nav>
-          </CollapsibleNav>
-        </Navbar>
+        <Navbar brand={<Link to='/'>Toni Karttunen</Link>} toggleNavKey={0}/>
       </header>
     );
   }
