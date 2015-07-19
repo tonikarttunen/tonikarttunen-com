e7485014cf2f3e591360b3215871c2b8d2f9382e
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-20 00:04:20 +0300

    Make <canvas> drawings look sharp on high-density ("Retina") screens

--- src/components/DiagonalDivider/DiagonalDivider.js
+++ src/components/DiagonalDivider/DiagonalDivider.js
@@ -14,8 +14,22 @@ export default class DiagonalDivider extends React.Component {
   componentDidMount() {
     let canvas = document.getElementById(this.props.id);
 
+    // Check if the <canvas> element exists and the browser supports <canvas>
     if (canvas && canvas.getContext) {
       let context = canvas.getContext('2d');
+
+      // Make canvas drawings look sharp on high-density ("Retina") screens
+      if ('devicePixelRatio' in window) {
+        if (window.devicePixelRatio > 1) {
+          const devicePixelRatio = window.devicePixelRatio;
+          canvas.width = canvas.width * devicePixelRatio;
+          canvas.height = canvas.height * devicePixelRatio;
+
+          // Update context for the new device pixel ratio
+          context = canvas.getContext('2d');
+        }
+      }
+
       context.strokeStyle = this.props.color;
       context.lineWidth = 2;
       context.moveTo(1, 61);
