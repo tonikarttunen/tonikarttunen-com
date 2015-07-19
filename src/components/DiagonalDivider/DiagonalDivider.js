88e7a0b8c3416555430fad28cbc1f27456596221
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-20 00:52:30 +0300

    Revert back to e043b80

--- src/components/DiagonalDivider/DiagonalDivider.js
+++ src/components/DiagonalDivider/DiagonalDivider.js
@@ -17,24 +17,10 @@ export default class DiagonalDivider extends React.Component {
     // Check if the <canvas> element exists and the browser supports <canvas>
     if (canvas && canvas.getContext) {
       let context = canvas.getContext('2d');
-
-      // Make canvas drawings look sharp on high-density ("Retina") screens
-      let devicePixelRatio = 1;
-      if ('devicePixelRatio' in window) {
-        if (window.devicePixelRatio > 1) {
-          devicePixelRatio = window.devicePixelRatio;
-          canvas.width = canvas.width * devicePixelRatio;
-          canvas.height = canvas.height * devicePixelRatio;
-
-          // Update context for the new device pixel ratio
-          context = canvas.getContext('2d');
-        }
-      }
-
       context.strokeStyle = this.props.color;
-      context.lineWidth = 2 * devicePixelRatio;
-      context.moveTo(1 * devicePixelRatio, 61 * devicePixelRatio);
-      context.lineTo(13 * devicePixelRatio, 1 * devicePixelRatio);
+      context.lineWidth = 2;
+      context.moveTo(1, 61);
+      context.lineTo(13, 1);
       context.stroke();
     }
   }
