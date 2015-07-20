cedb1257c3ac76e946c9eb5d70023871ff9eb016
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-07-21 02:29:24 +0300

    Update Viima project description

--- src/components/Viima/Viima.js
+++ src/components/Viima/Viima.js
@@ -31,23 +31,37 @@ export default class Viima {
               <p>Viima Solutions Oy</p>
               <h3>Awards</h3>
               <p>Accenture Quality Award Finalist</p>
+              <h3>Technology</h3>
+              <p>Backbone.JS</p>
             </Col>
             <Col md={8}>
               <p>
-                Goals
+                The goal of this project was to design and develop
+                a new user-friendly mobile user interface for Viima
+                from scratch. Viima is a browser-based tool that
+                can be used as a feedback system and as an internal
+                innovation platform within client companies.
               </p>
               <p>
-                Challenges: web UI only tailored for large screens,
-                difficult to handle it on tiny mobile screens;
-                had to redesign the UI from scratch
+                The main challenge of the project was that the playful
+                and highly visual desktop user interface of the Viima
+                system was tailored for large desktop computer screens.
+                We had to completely redesign the user interface for
+                mobile device maintaining the same playfulness as in
+                the desktop user interface. To get a realistic picture
+                of how the users of the application perceived our designs,
+                we conducted lots of user testing during the project
+                (first with paper prototypes and later with real software).
               </p>
               <p>
-                Good stuff: usability testing,
-                Accenture quality award finalist,
-                'ammattimainen ote tekemiseen' ja Scumin käyttö
-              </p>
-              <p>
-                Team members
+                The project was done as part Software Project course at
+                Aalto University. Our team, which consisted of seven students,
+                was selected as the second best team of the course by
+                Accenture and the course staff (there were 16 other teams
+                participating in the course). During the project, my tasks
+                included user interface design (both low-fidelity wireframing
+                and high-fidelity visual design with Adobe Illustrator),
+                usability testing, front-end software development and testing.
               </p>
             </Col>
           </Row>
