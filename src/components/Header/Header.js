67dc599847843956c378a91837a98ba89047be32
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-08-01 12:28:54 +0300

    Project detail view pages look visually OK

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,24 +5,18 @@ import { Link } from 'react-router';
 import { Grid } from 'react-bootstrap';
 import styles from './Header.less';
 import withStyles from '../../decorators/withStyles';
-import { staticPath } from '../../utilities/static/StaticPath';
 import MenuToggle from '../../components/MenuToggle';
 
 @withStyles(styles)
 export default class Header {
   render() {
-    const logoStyles = {
-      'background': 'url(' + staticPath('src/components/Header/images/Header-Logo-Light-Text.png') + ')',
-      'backgroundImage': 'url(' + staticPath('src/components/Header/images/Header-Logo-Light-Text.svg') + '), linear-gradient(transparent, transparent)'
-    };
-
     return (
       <header className='Header'>
         <nav className='NavigationBar' role='navigation'>
           <Grid>
             <div className='NavigationBarLeft'>
               <Link to='/' className='LogoContainer'>
-                <span className='Logo' style={logoStyles} />Toni Karttunen
+                <span className='Logo'/>Toni Karttunen
               </Link>
             </div>
             <div className='NavigationBarRight'>
--- src/components/StrategicUserCentredDesign/StrategicUserCentredDesign.js
+++ src/components/StrategicUserCentredDesign/StrategicUserCentredDesign.js
@@ -4,39 +4,42 @@ import React from 'react/addons';
 import { Grid, Row, Col } from 'react-bootstrap';
 import styles from './StrategicUserCentredDesign.less';
 import withStyles from '../../decorators/withStyles';
+import ProjectDetailView from '../../components/ProjectDetailView';
 
 @withStyles(styles)
 export default class StrategicUserCentredDesign {
   render() {
     return (
-      <section className="StrategicUserCentredDesign">
-        <Grid>
-          <h2>Internet of Things Solutions for Länsimetro</h2>
+      <ProjectDetailView>
+        <section className="StrategicUserCentredDesign">
+          <Grid>
+            <h2>Internet of Things Solutions for Länsimetro</h2>
 
-          <Row>
-            <Col md={4} className="InfoBox">
-              <h3>Spring</h3>
-              <p>2015</p>
-              <h3>Client</h3>
-              <p>Länsimetro</p>
-            </Col>
-            <Col md={8}>
-              <p>
-                Goals
-              </p>
-              <p>
-                Challenges: very little interaction with the client company (Länsimetro)
-              </p>
-              <p>
-                Good stuff usability testing, ideation (444 ideas), the whole SUCD process
-              </p>
-              <p>
-                Team members
-              </p>
-            </Col>
-          </Row>
-        </Grid>
-      </section>
+            <Row>
+              <Col md={4} className="InfoBox">
+                <h3>Spring</h3>
+                <p>2015</p>
+                <h3>Client</h3>
+                <p>Länsimetro</p>
+              </Col>
+              <Col md={8}>
+                <p>
+                  Goals
+                </p>
+                <p>
+                  Challenges: very little interaction with the client company (Länsimetro)
+                </p>
+                <p>
+                  Good stuff usability testing, ideation (444 ideas), the whole SUCD process
+                </p>
+                <p>
+                  Team members
+                </p>
+              </Col>
+            </Row>
+          </Grid>
+        </section>
+      </ProjectDetailView>
     );
   }
 }
