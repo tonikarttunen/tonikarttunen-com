26f6189b4fac7323f2fa28dda6984cd5b696ace5
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-09-15 00:29:17 +0300

    Support markdown formatting in blog posts

--- src/components/Blog/BlogDetail/BlogDetail.js
+++ src/components/Blog/BlogDetail/BlogDetail.js
@@ -5,7 +5,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import DocumentTitle from 'react-document-title';
 import styles from './BlogDetail.less';
 import withStyles from '../../../decorators/withStyles';
-import $ from 'jquery';
+const marked = require('marked');
 
 @withStyles(styles)
 export default class BlogDetail extends React.Component {
@@ -23,26 +23,39 @@ export default class BlogDetail extends React.Component {
 
   render() {
     const blogPost = {
-      title: 'Post 1',
-      date: '2015-10-01 15:00',
-      intro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      body_text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+      title: 'Post *1*',
+      date: {
+        weekDay: 'Monday',
+        year: 2015,
+        month: 10,
+        day: 1,
+        hours: 15,
+        minutes: 0,
+        seconds: 1.500
+      },
+      intro: 'Lorem __*ipsum*__ dolor *sit* amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.\n\nNemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
     };
 
+    const title = {__html: marked('# ' + blogPost.title)};
+    const intro = {__html: marked(blogPost.intro)};
+    const body = {__html: marked(blogPost.body)};
+    const documentTitle = blogPost.title.replace(/\_|\*/g, '') + ' — Blog — Toni Karttunen';
+
     return (
-      <DocumentTitle title={blogPost.title + ' — Blog — Toni Karttunen'}>
+      <DocumentTitle title={documentTitle}>
         <Grid className='Blog BlogDetail' componentClass='article'>
-          <h2>{blogPost.title}</h2>
+          <span dangerouslySetInnerHTML={title}/>
             <Row>
               <Col md={4}>
                 <p className='lead'>Published</p>
-                <p>{blogPost.date}</p>
+                <p>{blogPost.date.year}</p>
                 <p className='lead'>Author</p>
                 <p>Toni Karttunen</p>
               </Col>
               <Col md={8}>
-                <p className='lead'>{blogPost.intro}</p>
-                <p>{blogPost.body_text}</p>
+                <span className='lead' dangerouslySetInnerHTML={intro}/>
+                <span dangerouslySetInnerHTML={body}/>
               </Col>
             </Row>
         </Grid>
--- src/components/Blog/BlogMaster/BlogMaster.js
+++ src/components/Blog/BlogMaster/BlogMaster.js
@@ -6,6 +6,7 @@ import DocumentTitle from 'react-document-title';
 import { Link } from 'react-router';
 import styles from './BlogMaster.less';
 import withStyles from '../../../decorators/withStyles';
+const marked = require('marked');
 
 @withStyles(styles)
 export default class BlogMaster extends React.Component {
@@ -24,15 +25,15 @@ export default class BlogMaster extends React.Component {
   render() {
     const blogPosts = [
       {
-        title: 'Post 1',
+        title: 'Post *1*',
         date: '2015-10-01 15:00',
-        intro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        intro: 'Lorem __*ipsum*__ dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         url: '/2015/10/01/recording-mobile-devices-in-usability-tests'
       },
       {
         title: 'Post 2',
         date: '2015-10-01 12:00',
-        intro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        intro: 'Lorem ipsum dolor __sit__ amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         url: '/2015/10/01/second-post'
       },
       {
@@ -46,8 +47,11 @@ export default class BlogMaster extends React.Component {
     return (
       <DocumentTitle title='Blog — Toni Karttunen'>
       <Grid className='Blog BlogMaster' componentClass='article'>
-        <h2>Blog</h2>
+        <h1>Blog</h1>
         {blogPosts.map((blogPost) => {
+          const title = {__html: marked('## ' + blogPost.title)};
+          const intro = {__html: marked(blogPost.intro)};
+
           return (
             <Row key={blogPost.url}>
               <Col md={4}>
@@ -57,9 +61,9 @@ export default class BlogMaster extends React.Component {
                 <p>{blogPost.date}</p>
               </Col>
               <Col md={8}>
-                <Link to={blogPost.url}><h3>{blogPost.title}</h3></Link>
-                <p className='lead'>{blogPost.intro}</p>
-                <p><Link to={blogPost.url} className='MoreInformation'>More Information</Link></p>
+                <Link to={blogPost.url}><span dangerouslySetInnerHTML={title}/></Link>
+                <span className='lead' dangerouslySetInnerHTML={intro}/>
+                <p><Link to={blogPost.url} className='MoreInformation'>Read More</Link></p>
               </Col>
             </Row>
           );
