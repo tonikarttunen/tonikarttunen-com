abfd50121d200f77bae42d75014ec2bb705c06b2
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-09-15 04:01:11 +0300

    Stylistic changes

--- src/components/Blog/BlogMaster/BlogMaster.js
+++ src/components/Blog/BlogMaster/BlogMaster.js
@@ -38,19 +38,19 @@ export default class BlogMaster extends React.Component {
   render() {
     const blogPosts = [
       {
-        title: 'Post *1*',
+        title: 'Post *1*: Lorem Ipsum Dolor Sit Amet',
         date: '2015-10-01 15:00',
         intro: 'Lorem __*ipsum*__ dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         url: '/blog/2015/10/01/recording-mobile-devices-in-usability-tests'
       },
       {
-        title: 'Post 2',
+        title: 'Post 2: Consectetur Adipiscing Elit',
         date: '2015-10-01 12:00',
         intro: 'Lorem ipsum dolor __sit__ amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         url: '/blog/2015/10/01/second-post'
       },
       {
-        title: 'Post 3',
+        title: 'Post 3: Sed Do Eiusmod Tempor',
         date: '2015-10-01 09:00',
         intro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         url: '/blog/2015/10/01/third-post'
