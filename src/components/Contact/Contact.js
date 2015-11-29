c0a4670b4a185dc25d6c2afae49aa92488b25829
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2015-11-29 11:54:59 +0200

    Move HTTP request to componentDidMount in Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,83 +9,87 @@ import { supportsSVG, isIE10OrOlder } from '../../utilities/FeatureDetection/Fea
 import $ from 'jquery';
 const request = require('superagent');
 
+const socialMediaImageBasePath = 'src/components/Contact/images/';
+
+const socialMedia = [
+  {
+    title: 'Behance',
+    url: 'https://www.behance.net/tonikarttunen',
+    imageFilename: 'behance'
+  },
+  {
+    title: 'GitHub',
+    url: 'https://github.com/tonikarttunen',
+    imageFilename: 'github'
+  },
+  {
+    title: 'Medium',
+    url: 'https://medium.com/@tonikarttunen',
+    imageFilename: 'medium'
+  },
+  {
+    title: 'Twitter',
+    url: 'https://twitter.com/tonikarttunen',
+    imageFilename: 'twitter'
+  },
+  {
+    title: 'Instagram',
+    url: 'http://instagram.com/tonikarttunen',
+    imageFilename: 'instagram'
+  },
+  {
+    title: '500px',
+    url: 'http://500px.com/tonikarttunen',
+    imageFilename: '500px'
+  }
+];
+
 @withStyles(styles)
-export default class Contact {
+export default class Contact extends React.Component {
+  componentDidMount() {
+    if (supportsSVG() && !isIE10OrOlder()) {
+      socialMedia.map(media => {
+        request
+        .get(staticPath(socialMediaImageBasePath + 'SVG/' + media.imageFilename + '.svg'))
+        .end((err, res) => {
+          if (!err) {
+            $('#ContactSocialMedia' + media.title).html(res.text);
+          }
+        });
+      });
+    }
+  }
+
   render() {
-    const socialMedia = [
-      {
-        title: 'Behance',
-        url: 'https://www.behance.net/tonikarttunen',
-        imageFilename: 'behance'
-      },
-      {
-        title: 'GitHub',
-        url: 'https://github.com/tonikarttunen',
-        imageFilename: 'github'
-      },
-      {
-        title: 'Medium',
-        url: 'https://medium.com/@tonikarttunen',
-        imageFilename: 'medium'
-      },
-      {
-        title: 'Twitter',
-        url: 'https://twitter.com/tonikarttunen',
-        imageFilename: 'twitter'
-      },
-      {
-        title: 'Instagram',
-        url: 'http://instagram.com/tonikarttunen',
-        imageFilename: 'instagram'
-      },
-      {
-        title: '500px',
-        url: 'http://500px.com/tonikarttunen',
-        imageFilename: '500px'
-      }
-    ];
-
-    const socialMediaImageBasePath = 'src/components/Contact/images/';
-
     return (
       <Grid componentClass='section' className='Contact'>
         <Row>
           {
-            socialMedia.map(
-              (media) => {
-                return (
-                  <a key={media.title} href={media.url}>
-                    <Col md={3} sm={6} className='ContactRow'>
-                      <div id={'ContactSocialMedia' + media.title} className='InlineBlock IconContainer'>
-                        {
-                          (() => {
-                            if (supportsSVG() && !isIE10OrOlder()) {
-                              request
-                              .get(staticPath(socialMediaImageBasePath + 'SVG/' + media.imageFilename + '.svg'))
-                              .end((err, res) => {
-                                if (!err) {
-                                  $('#ContactSocialMedia' + media.title).html(res.text);
-                                }
-                              });
-                            } else {
-                              return (
-                                <img
-                                  src={staticPath(socialMediaImageBasePath + 'PNG/' + media.imageFilename + '.png')}
-                                  alt=''
-                                />
-                              )
-                            }
-                          })()
-                        }
-                      </div>
-                      <div className='InlineBlock TextContainer'>
-                        {media.title}
-                      </div>
-                    </Col>
-                  </a>
-                );
-              }
-            )
+            socialMedia.map(media => {
+              return (
+                <a key={media.title} href={media.url}>
+                  <Col md={3} sm={6} className='ContactRow'>
+                    <div id={'ContactSocialMedia' + media.title} className='InlineBlock IconContainer'>
+                      {
+                        (() => {
+                          if (!supportsSVG() || isIE10OrOlder()) {
+                            return (
+                              <img
+                                src={staticPath(socialMediaImageBasePath + 'PNG/' + media.imageFilename + '.png')}
+                                alt=''
+                              />
+                            ) 
+                          }
+                        })()
+                      }
+                    </div>
+                    <div className='InlineBlock TextContainer'>
+                      {media.title}
+                    </div>
+                  </Col>
+                </a>
+              );
+            })
           }
         </Row>
       </Grid>
